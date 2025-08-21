c1d80d9078b0d0f0dc997481ba93ececb657d57c
ShemLeon <shemiakinleonid@gmail.com>
2025-08-21 21:05:14 +0300

    Eto uspeh

--- app/src/main/java/com/leoevg/gini/data/mapper/PixabayServerDataHandler.kt
+++ app/src/main/java/com/leoevg/gini/data/mapper/PixabayServerDataHandler.kt
@@ -11,8 +11,8 @@ object PixabayServerDataHandler {
                 && hit.likes != null
                 && hit.comments != null
                 && hit.image != null
-                && hit.likes > 50
-                && hit.comments > 50
+                && hit.likes > 5
+                && hit.comments > 5
             ) {
                 CardUI(
                     id = hit.id,
